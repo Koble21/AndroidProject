8289173da577f54c60d8c2b2737ca735bf794ccc
Koble21 <62141290+Koble21@users.noreply.github.com>
2021-02-07 22:58:01 +0200

    Register&Update
    Register and update fragments implementing, and a lot, and a lot of problems do debug ..

--- app/src/main/java/com/example/restaurantproject/ProfileFragment.kt
+++ app/src/main/java/com/example/restaurantproject/ProfileFragment.kt
@@ -5,8 +5,8 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
-import android.media.Image
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -16,8 +16,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.restaurantproject.data.User
 import com.example.restaurantproject.data.UserViewModel
 import com.google.android.material.snackbar.Snackbar
@@ -27,99 +30,44 @@ import java.io.ByteArrayOutputStream
 class ProfileFragment : Fragment() {
     private lateinit var mUserViewModel: UserViewModel
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-        val chooseB=view.findViewById<Button>(R.id.chooseButton)
-        val image=view.findViewById<ImageView>(R.id.imageChoosed)
-        val insert=view.findViewById<Button>(R.id.insertButton)
-        val name = view.findViewById<EditText>(R.id.personName)
-        val address = view.findViewById<EditText>(R.id.postalAdress)
-        val number = view.findViewById<EditText>(R.id.phoneNumber)
-        val email = view.findViewById<EditText>(R.id.emailAdress)
-        chooseB.setOnClickListener{
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if(ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
-                {
-//                    permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-//                    show popup to request runtime permission
-                    requestPermissions(permissions, PERMISSION_CODE)
-                }
-                else
-                {
-//                    permission already granted
-                    pickImageFromGallery()
-                }
-            }
-            else
-            {
-//                system OS is < Marshmalow
-                pickImageFromGallery()
-            }
-        }
-        insert.setOnClickListener{
-            if (name.length() ==0||address.length() ==0|| email.length() ==0||number.length() ==0)
-                Snackbar.make(this.requireView(), "Fill the fields correctly!", Snackbar.LENGTH_SHORT).show()
-            else
-            {
-                val picture:ByteArray=imageViewToByte(image)
+        val image = view.findViewById<ImageView>(R.id.imageChoosed)
+        val name = view.findViewById<TextView>(R.id.personName)
+        val address = view.findViewById<TextView>(R.id.postalAdress)
+        val number = view.findViewById<TextView>(R.id.phoneNumber)
+        val email = view.findViewById<TextView>(R.id.emailAdress)
+        val update=view.findViewById<Button>(R.id.update_Button)
+        val register=view.findViewById<Button>(R.id.register_Button)
 
-                val user = User(0, name.text.toString(), address.text.toString(), number.text.toString(), email.text.toString(),picture)
-                mUserViewModel.deleteTheUsers()
-                mUserViewModel.addUser(user)
-                Snackbar.make(this.requireView(), "Data successfully added :D !", Snackbar.LENGTH_SHORT).show()
+        mUserViewModel.readUserData().observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                name.text = it.name.toString()
+                address.text = it.address.toString()
+                email.text = it.email.toString()
+                number.text = it.phone.toString()
+                if(it.image?.size != null)
+                {
+                    image.setImageBitmap(BitmapFactory.decodeByteArray(it.image, 0, it.image?.size!!))
+                }
             }
+        })
+
+        update.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_updateFragment)
         }
+
+        register.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_registerFragment)
+        }
+
+
         return view
 
     }
-    private fun imageViewToByte(image: ImageView): ByteArray {
-        val bitmap = (image.drawable as BitmapDrawable).bitmap
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
-
-        return stream.toByteArray()
-    }
-
-    private fun pickImageFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
-    }
-
-    companion object{
-        private val IMAGE_PICK_CODE = 1000
-        private val PERMISSION_CODE = 1001
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode){
-            PERMISSION_CODE -> {
-                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    //permission from popup granted
-                    pickImageFromGallery()
-                }
-                else
-                {
-                    //permission from popup denied
-                    Snackbar.make(this.requireView(), "Permission denied!", Snackbar.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val image = view?.findViewById<ImageView>(R.id.imageChoosed)
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            image?.setImageURI(data?.data)
-        }
-    }
-
-
 }
